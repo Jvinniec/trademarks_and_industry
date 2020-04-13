bed56fba432a505773a17816d8eabfd401b92319
Josh Cardenzana <jcardenzana@irap.omp.eu>
2020-04-13 23:55:39 +0200

    Update XML parser class in 'load_xml.py'

--- data/trademark_data.md
+++ data/trademark_data.md
@@ -1,20 +1,25 @@
+The data used in this project is broken into 
+
 # Trademark Filing Data
 The trademark filing data can be downloaded from the United States Patents and Trademarks Office (USPTO) here:
 * Historical: https://developer.uspto.gov/product/trademark-annual-xml-applications
 * Daily: https://developer.uspto.gov/product/trademark-daily-xml-file-tdxf-applications#product-files
 
 
-# XML format
+## XML format
 The data that will be extracted for each trademark filing is found in each file based on the following XML file structure.
 ```xml
 <trademark-applications-daily>
   <application-information>
     <file-segments>
       <action-keys>
+
         <case-file>
           <serial-number>######</serial-number>
           <case-file-header>
             <filing-date>YYYYMMDD</filing-date>
+            <registration-date>YYYYMMDD</registration-date>
+            <status-code>###</status-code>
             <mark-identification>TEXT</mark-identification>
           </case-file-header>
           <case-file-statements>
@@ -28,8 +33,19 @@ The data that will be extracted for each trademark filing is found in each file 
             </classification>
             <!-- ... additional classifications ... -->
           </classifications>
+          <case-file-owners>
+            <case-file-owner>
+              <!-- ... Filer information -->
+              <city>New York</city>
+              <state>NY</state>
+              <!-- alternatively: country code -->
+            </case-file-owner>
+            <!-- additional case-file-owner groups -->
+          </case-file-owners>
         </case-file>
+
         <!-- ... additional case files ... -->
+
       </action-keys>
     </file-segments>
   </application-information>
@@ -38,7 +54,21 @@ The data that will be extracted for each trademark filing is found in each file 
 
 The following data are expected to be extracted from each filing:
 * filing-date: Date on which the trademark application was filed
+* registration-date: Date the trademark was officially registered
+* status-code: Current status of the trademark
 * serial-number: Unique identification number associated with the trademark
 * mark-identification: Short name for the trademark
 * case-file-statement/text: Short description of the trademark (could have more than one)
-* classification/primary-code: Nice classification associated with the trademark (could be more than 1) 
+* classification/primary-code: List of Nice classifications associated with the trademark
+* case-file-owner:
+  * city, state, country
+
+
+# Nice Classifications
+The International Classification of Goods and Services (a.k.a. Nice Classification) identifies 45 groups into which a trademark may be classified.
+* [Nice Classification details](https://www.wipo.int/classifications/nice/nclpub/en/fr/?explanatory_notes=show&lang=en&menulang=en&notion=class_headings&version=20200101) (as of Jan. 1, 2020)
+
+For a list of Nice classifications, see [help_files/nice_classifications.csv](help_files/nice_classifications.csv).
+
+For a list of industries associated with each Nice classification, see [help_files/nice_industry.csv](help_files/nice_industry.csv)
+
--- tm_helper/load_xml.py
+++ tm_helper/load_xml.py
@@ -1,126 +1,227 @@
 
 
 # Import XML parsing module
-import xml.etree.ElementTree as ET
 import pandas as pd
 import datetime as dt
+import re
+
+class TmParser():
+
+    def __init__(self, verbose=True):
+        self.verbose = verbose
+
+    def parse_text(self, tag, text):
+        """ Parse out string between a given `<tag>text</tag>` text
+
+        Parameters
+        ----------
+        tag : str
+            XML tag name
+        text : str
+            text to extract from
+        """
+        # Define the regex
+        rtag = f'.*?<{tag}>(.*?)</{tag}>'
+        regex = re.compile(rtag)
+
+        # Join all matches into a single string
+        return regex.match(text).groups()        
 
 
-def load_cases(filename):
-    """
-    Load data from an xml object downloaded from the USPTO website
-
-    Parameters
-    ----------
-    filename: str
-        Name of the xml file to read from
-    """
-    # Extract the tree from the file
-    tree = ET.parse(filename)
-    
-    # Drill down to the case files
-    app_info = tree.getroot().find('application-information')
-    file_seg = app_info.find('file-segments')
-    act_keys = file_seg.find('action-keys')
-    
-    # Return the actual case files
-    return act_keys.findall('case-file')
-
-
-def col_names():
-    """
-    """
-    # Define the column names
-    col_names = ['fileDate','serialNum','markId','descrip']
-    for i in range(1,46):
-        col_names.append(f'{i:03d}')
-    return col_names
-
-
-def col_dict():
-    """ Return a dict object with associated columns
-    """
-    # Get the column names
-    cols = col_names()
-    
-    # Create the return dict object
-    ret = dict()
-    for name in cols:
-        ret[name] = 0
-    return ret
-
-
-def init_dataframe():
-    """
-    Build the DataFrame
-    """
-    #
-    df = pd.DataFrame(columns=col_names())
-    return df
-
-
-def parse_cases(filename):
-    """
-    Parse the case files into individual entries in a Pandas.DataFrame
-    """
-    # Load the individual case files
-    #tree  = load_xml(filename)
-    cases = load_cases(filename)
-
-    # Initialize the dataframe to store the result
-    df = init_dataframe()
-
-    # Loop through all the cases
-    cnt = 0
-    for case in cases:
+    def parse_case(self, case_txt):
+        """ Parse the case text 
+        
+        Parameters
+        ----------
+        case_txt : str
+            String of text to be parsed
+        """
         # Get the default column name
-        row = col_dict()
+        row = self.col_dict()
 
-        # Serial number
-        row['serialNum'] = int(case.find('serial-number').text)
-
-        # Filing date (may not be valid)
-        header = case.find('case-file-header')
+        # ==========
+        # Serial number (this is mandatory for all trademarks)
+        # ==========
+        row['serialNum'] = int(self.parse_text('serial-number', case_txt)[0])
+        
+        # ==========
+        # Status code
+        # ==========
         try:
-            date_time_str = header.find('filing-date').text
+            status = int(self.parse_text('status-code', case_txt)[0])
+
+            # Handle bad status codes
+            if status in []:
+                return None
+            else:
+                row['status'] = status
+        except:
+            row['status'] = None
+
+        # ==========
+        # Filing date (may not be valid)
+        # ==========
+        try:
+            date_time_str = self.parse_text('filing-date', case_txt)[0]
+            #row['fileDate'] = date_time_str
             row['fileDate'] = dt.datetime.strptime(date_time_str, '%Y%m%d')
         except:
             row['fileDate'] = None
 
-        # Extract mark ID (may not be present)
+        # ==========
+        # Registration date (may not be valid)
+        # ==========
         try:
-            raise ValueError
-            row['markId'] = header.find('mark-identification').text
+            date_time_str = self.parse_text('registration-date', case_txt)[0]
+            row['registrationDate'] = dt.datetime.strptime(date_time_str, '%Y%m%d')
+        except:
+            row['registrationDate'] = None
+
+        # ==========
+        # Extract mark ID (may not be present)
+        # ==========
+        try:
+            #raise ValueError
+            row['markId'] = self.parse_text('mark-identification', case_txt)[0]
         except:
             row['markId'] = None
 
+        # ==========
         # Statements
+        # ==========
         try:
-            raise ValueError
-            statements = case.find('case-file-statements').findall('case-file-statement')
-            row['descrip'] = ' '.join([statement.find('text').text for statement in statements])
+            stmts = self.parse_text('case-file-statement', case_txt)
+            stmts_txt = '; '.join([self.parse_text('text',s)[0] for s in stmts])
+            row['descrip'] = stmts_txt
         except:
             row['descrip'] = None
 
-        # Classifications
+        # ==========
+        # Location
+        # ==========
+        
         try:
-            classes = case.find('classifications').findall('classification')
+            # Get the case-owner text (just get the first)
+            case_owner = self.parse_text('case-file-owner', case_txt)[0]
+
+            # City
+            row['city'] = self.parse_text('city', case_owner)[0]
+
+            # State
+            regex = re.compile(f'.*</city><state>(.*)</state>')
+            m = regex.search(case_owner)
+            if m:
+                row['state'] = m.groups()[0]
+            else:
+                row['state'] = None
+
+            # Country
+            regex = re.compile(f'.*</city><country>(.*)</country>')
+            m = regex.search(case_owner)
+            if m:
+                row['country'] = m.groups()[0]
+            else:
+                row['country'] = 'US'
+        except:
+            # No case owner found
+            row['city']    = None
+            row['state']   = None
+            row['country'] = None
+
+        # ==========
+        # Classifications
+        # ==========
+        try:
+            #classes = case.select('classifications classification')
+            classes = self.parse_text('classification', case_txt)
+            clss_list = []
             for clss in classes:
                 # Sometimes the code doesn't make sense, so skip it when it doesn't
-                clss_txt = clss.find('primary-code').text
-                if clss_txt in row:
-                    row[clss_txt] = 1
+                #clss_int = int(clss.find('primary-code').text)
+                clss_int = int(self.parse_text('primary-code', clss)[0])
+                if clss_int > 0 and clss_int < 46:
+                    clss_list.append(clss_int)
+            
+            row['niceClass'] = clss_list
         except:
             # No associated classifications
-            pass
+            row['niceClass'] = None
 
-        cnt += 1
-        if cnt % 100 == 0:
-            print(row['serialNum'])
+        return row
+
+
+    def parse_cases(self, filename):
+        # Initialize the dataframe to store the result
+        data = dict()
+
+        # Loop through all the cases
+        cnt = 0
+
+        # Various regex catches
+        open_tag = re.compile(r'<case-file>')
+        close_tag = re.compile(r'</case-file>')
         
-        # Append the row using the serial number as the index
-        df.loc[row['serialNum']] = row
+        # Loop through the file
+        with open(filename, 'r') as f:
+            case_txt = ''
+            for line in f:
+                line = line.strip()
+                
+                # Check if open tag is on the line
+                if open_tag.match(line):
+                    case_txt = ''
+
+                # Check if open tag is on the line
+                elif close_tag.match(line):
+                    row = self.parse_case(case_txt)
+                    
+                    if row is not None:
+                        # Append the row using the serial number as the index
+                        serialNum = row.pop('serialNum')
+                        data[serialNum] = row
+
+                    cnt += 1
+                    if (cnt % 100 == 0) and self.verbose:
+                        print(f'\rProcessed: {cnt: 8}', end='', flush=True)
+                else:
+                    case_txt += line
+
+        # Write the final number processed
+        if self.verbose:
+            print(f'\rFINAL processed: {cnt}')
+
+        #df = pd.DataFrame.from_dict(df, orient='index')
+
+        return data
+
+
+    def col_names(self):
+        """
+        """
+        # Define the column names
+        col_names = ['fileDate','registrationDate','status','serialNum','markId','descrip','niceClass','city','state','country']
+        # for i in range(1,46):
+        #     col_names.append(f'{i:03d}')
+        return col_names
+
+
+    def col_dict(self):
+        """ Return a dict object with associated columns
+        """
+        # Get the column names
+        cols = self.col_names()
         
-        
-    return df
- 
+        # Create the return dict object
+        ret = dict()
+        for name in cols:
+            ret[name] = 0
+        return ret
+
+
+    def init_dataframe(self):
+        """
+        Build the DataFrame
+        """
+        #
+        df = pd.DataFrame(columns=self.col_names())
+        return df
